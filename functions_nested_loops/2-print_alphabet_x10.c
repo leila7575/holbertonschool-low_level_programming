64f6c95135eb693b27f85ae764debe80c6fc3665
leila7575 <leila.louajri@gmail.com>
2023-10-19 06:04:52 -0700

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function that prints the alphabet 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	int repeat;
+	char alphabet;
+
+	while (repeat++ < 10)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		_putchar(alphabet);
+	}
+		_putchar('\n');
+}
