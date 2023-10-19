4be239ac6232ab9487ced72253b3df19d42fae0a
leila7575 <leila.louajri@gmail.com>
2023-10-19 06:19:11 -0700

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -12,7 +12,7 @@ void print_alphabet_x10(void)
 	while (repeat++ < 10)
 	{
 		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-		_putchar(alphabet);
-	}
+			_putchar(alphabet);
 		_putchar('\n');
+	}
 }
