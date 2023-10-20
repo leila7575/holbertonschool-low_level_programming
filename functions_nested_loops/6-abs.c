fa5c3b39cbbbf858fb510b9baac5c2590a679eb3
leila7575 <leila.louajri@gmail.com>
2023-10-20 03:07:50 -0700

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value
+ * @r: number to compute
+ * Return: r or -r
+ */
+
+int _abs(int r)
+{
+	if (r < 0)
+	{
+		return (-r);
+	}
+	else
+	{
+		return (r);
+	}
+}
