88f93d281097738f4847221eb15be2d2aa62cbf6
leila7575 <leila.louajri@gmail.com>
2023-10-22 05:05:55 -0700

    5-more_numbers

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -15,10 +15,9 @@ void more_numbers(void)
 		for (x = 0 ; x <= 14 ; x++)
 		{
 			if (x >= 10)
-			{
-				_putchar ('1');
-				_putchar (x % 10 + '0');
-			}
+			_putchar ('1');
+			²_putchar (x % 10 + '0');
+	
 		}
 		_putchar('\n');
 
