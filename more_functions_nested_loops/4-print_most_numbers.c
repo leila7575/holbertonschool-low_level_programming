7c4eb17b9e3970b1709cc903d4d67f2c5d7e08a7
leila7575 <leila.louajri@gmail.com>
2023-10-22 02:41:04 -0700

    3-print_number

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers
+ * Return: numbers from 0 to 9
+ */
+void print_most_numbers(void)
+{
+	int i;
+	while (i!=2 && i!=4)
+	{
+		for (i=0; i<=9; i++)
+		{
+			_putchar (i);
+		}
+		_putc
+	}
+}
+                                    
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,6 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -11,5 +10,3 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-
-#endif
