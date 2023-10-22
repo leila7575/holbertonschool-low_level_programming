cfd9a8b32cc404677811aff47add1f5233b6cc91
leila7575 <leila.louajri@gmail.com>
2023-10-22 11:40:17 -0700

    9-times_table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -2,35 +2,32 @@
 
 /**
  * times_table - prints the 9 times table
- * Return:
  *
  */
-
 void times_table(void)
 {
-	int a, b;
+	int a, b, result;
 
-	for (a = 0; b < 10; a++)
+	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b < 10; b++)
 		{
-			if (b == 0)
+			result = a * b;
+
+			if (b > 0)
+				_putchar(',');
+
+			if (result < 10)
 			{
-				_putchar('0');
-			}
-			else if (a * b < 10)
-			{
-				_putchar(44);
-				_putchar(32);
-				_putchar(32);
-				_putchar(a * b + '0');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(result + '0');
 			}
 			else
 			{
-				_putchar(44);
-				_putchar(32);
-				_putchar((a * b) / 10 + '0');
-				_putchar((a * b) % 10 + '0');
+				_putchar(' ');
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
 			}
 		}
 		_putchar('\n');
