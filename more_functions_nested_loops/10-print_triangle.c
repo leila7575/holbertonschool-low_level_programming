92ec924351a15a3cb5383e28f1567b98aa283ce7
leila7575 <leila.louajri@gmail.com>
2023-10-22 06:57:38 -0700

    10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle: prints a triangle
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
+{
+	int row, space, hash;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 0; row < size; row++)
+		{
+			for (space = 0; space < size - row - 1; space++)
+			{
+				_putchar(' ');
+			}
+			for (hash = 0; hash < row + 1; hash++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
