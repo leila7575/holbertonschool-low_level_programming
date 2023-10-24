70ca2e47d8a601888edf6521d299e5100d490458
leila7575 <leila.louajri@gmail.com>
2023-10-24 09:42:06 -0700

    6-puts2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,13 +10,10 @@ void puts2(char *str)
 {
 	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i += 2)
 	{
-		for (i = 0; i % 2 == 0; i++)
-		{
-			_putchar(str[i]);
-		}
-		_putchar('\n');
+		_putchar(str[i]);
 	}
+	_putchar('\n');
 }
 
