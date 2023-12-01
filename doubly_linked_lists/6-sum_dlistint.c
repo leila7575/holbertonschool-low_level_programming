32a7a70fa6edffc5473b468cd71f15cfb341415c
leila7575 <leila.louajri@gmail.com>
2023-12-01 11:35:15 -0800

    6-sum_dlistint.c

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- * sum_dlistint -  returns the sum of all the data (n) of a dlistint_t linked list
+ * sum_dlistint -  returns the sum of all the data
  * @head: pointer to the start of the list
  * Return: sum of all data or 0 if the list is empty
  */
