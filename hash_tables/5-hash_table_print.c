d956c720a30b3fc659c16b05e246a44d8507b53c
leila7575 <leila.louajri@gmail.com>
2023-12-08 12:21:46 -0800

    5-hash_table_print.c

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ * Return:
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *temp;
+	char flag = 0;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		while (temp != NULL)
+		{
+		if (flag == 1)
+			printf("'%s' : '%s'", temp->key, temp->value);
+		temp = temp->next;
+		flag = 1;
+		}
+	}
+	printf("}\n");
+}
