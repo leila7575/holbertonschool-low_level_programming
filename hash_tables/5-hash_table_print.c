5351e5b9a2c6e9119a5c1c96345d3c5fa120d9c0
leila7575 <leila.louajri@gmail.com>
2023-12-08 14:32:57 -0800

    5-hash_table_print.c

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -7,7 +7,7 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int i, j;
+	unsigned long int i, j = 0;
 	hash_node_t *temp;
 
 	if (ht == NULL)
@@ -17,14 +17,17 @@ void hash_table_print(const hash_table_t *ht)
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
-		temp = ht->array[i];
-		while (temp != NULL)
+		if (ht->array[i] != NULL)
 		{
-			if (j > 0)
-				printf(", ");
-			printf("'%s' : '%s'", temp->key, temp->value);
-			temp = temp->next;
-			j++;
+			temp = ht->array[i];
+			while (temp != NULL)
+			{
+				if (j > 0)
+					printf(", ");
+				printf("'%s' : '%s'", temp->key, temp->value);
+				temp = temp->next;
+				j++;
+			}
 		}
 	}
 	printf("}\n");
