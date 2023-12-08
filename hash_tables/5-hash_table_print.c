bb38b6155bd346409b04256e1e8098910ac7c04a
leila7575 <leila.louajri@gmail.com>
2023-12-08 12:29:45 -0800

    5-hash_table_print.c

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -23,7 +23,7 @@ void hash_table_print(const hash_table_t *ht)
 		{
 		if (flag == 1)
 			printf(", ");
-			printf("'%s' : '%s'", temp->key, temp->value);
+		printf("'%s' : '%s'", temp->key, temp->value);
 		temp = temp->next;
 		flag = 1;
 		}
