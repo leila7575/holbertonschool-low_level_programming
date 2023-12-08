df0d7f6afbc6082722284f3f54edc122a89bdb51
leila7575 <leila.louajri@gmail.com>
2023-12-07 16:12:03 -0800

    3-cp.c

---
+++ file_io/3-cp.c
@@ -0,0 +1,70 @@
+#include "main.h"
+
+#define USAGE "Usage: cp file_from file_to\n"
+#define READ_ERROR "Error: Can't read from file %s\n"
+#define WRITE_ERROR "Error: Can't write to %s\n"
+#define CLOSE_ERROR "Error: Can't close fd %d\n"
+
+#define BUFFER_SIZE 1024
+
+/**
+ * main - program that copies the content of a file to another
+ * @argc: number of arguments
+ * @argv: array of pointers to the arguments
+ * Return: 1 on success, 0 on failure
+ */
+
+int main(int argc, char *argv[])
+{
+	const char *file_from = argv[1];
+	const char *file_to = argv[2];
+	char buffer[BUFFER_SIZE];
+	ssize_t rbytes, wbytes;
+	int fd_from, fd_to;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, USAGE), exit(97);
+	}
+
+	fd_from = open(file_from, O_RDONLY);
+
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, READ_ERROR, file_from), exit(98);
+	}
+
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+
+	if (fd_to == -1)
+	{		
+		close(fd_from);
+		dprintf(STDERR_FILENO, WRITE_ERROR, file_to), exit(99);
+	}
+
+	while ((rbytes = read(fd_from, buffer,  BUFFER_SIZE)) > 0)
+	{
+		wbytes = write(fd_to, buffer, rbytes);
+		if (wbytes != rbytes)
+		{
+			close(fd_from);
+			close(fd_to);
+			dprintf(STDERR_FILENO, WRITE_ERROR, file_to), exit(99);
+		}
+	}
+
+	if (rbytes == -1)
+	{
+		close(fd_from);
+		close(fd_to);
+		dprintf(STDERR_FILENO, READ_ERROR, file_from), exit(98);
+	}
+
+	if (close(fd_from) == -1 || close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, CLOSE_ERROR, fd_from), exit(100);
+	}
+
+	return(0);
+}
+
--- file_io/main.h
+++ file_io/main.h
@@ -6,7 +6,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 
+int main(int argc, char *argv[]);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
